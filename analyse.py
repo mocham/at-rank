12ede03c7f3bf34fce5a6eabbb3f9ab035b00134
mocham <45742107+mocham@users.noreply.github.com>
2018-12-30 18:14:39 -0500

    Add files via upload

--- analyse.py
+++ analyse.py
@@ -1,5 +1,6 @@
 import pickle, operator
 from math import sqrt
+import json
 
 class at_card:
     def __init__(self, atk, hp):
@@ -10,7 +11,13 @@ class at_card:
     def prt(self):
         print([self.atk, self.hp, self.trait, self.skills])
 
-chs = ['bill-dauterive',
+current_BGE = []#['educated']#['eduacated', 'addicted']
+
+chs = [
+        'bullock', 'chris', 'dale', 'dr-zoidberg', 'fry', 'hank', 'linda', 'lois', 'mort', 'stevs',
+        'bill', 'gene', 'hayley', 'hermes', 'klaus', 'luanns', 'meg', 'professor-farnsworth', 'quagmire', 'teddy', 
+        'amy', 'bob', 'boomhauer', 'brian', 'francine', 'leela', 'louise', 'peggy', 'stan', 'stewie',        
+        'bill-dauterive',
         'philip-j-fry',
         'dr-amy-wong',
         'eugene-belcher',
@@ -25,6 +32,7 @@ chs = ['bill-dauterive',
         'linda-belcher',
         'klaus-heisler',
         'bobby',
+        'francine-smith',
         'lois-griffin',
         'hermes-conrad',
         'mythic-stewie',
@@ -43,7 +51,235 @@ chs = ['bill-dauterive',
         'mythic-dr-zoidberg',
         'mythic-professor-farnsworth',
         'mythic-bender',
+        'mythic-bobby',
+        'mythic-peter',
+        'mythic-tina',
         ]
+
+stats = {
+        'bullock':[
+            [0,0], [3,15], [0,0],
+        ],
+        'chris':[
+            [0,0], [3,13],[0,0],
+        ],
+        'dale': [
+            [0,0], [2, 13],[0,0],
+        ],
+        'dr-zoidberg':[
+            [0,0], [2,15],[0,0],
+        ],
+        'fry': [
+            [0,0],[6,10],[0,0],
+        ],
+        'hank':[
+            [0,0],[5,8],[0,0],
+        ],
+        'linda':[
+            [0,0],[3,13],[0,0],
+        ],
+        'lois':[
+            [0,0],[5,10],[0,0],
+        ],
+        'mort': [
+            [0,0], [5,10],[0,0],
+        ],
+        'stevs':[
+            [0,0], [4,12], [0,0],
+        ],
+        'bill': [
+            [0,0],[7,13],[0,0],
+        ],
+        'gene':[
+            [0,0],[4,16],[0,0],
+        ],
+        'hayley':[
+            [0,0],[5,15], [0,0],
+        ],
+        'hermes':[
+            [0,0],[5,16],[0,0],
+        ],
+        'klaus':[
+            [0,0],[4,15], [0,0],
+        ],
+        'luanns':[
+            [0,0],[7,16],[0,0],
+        ],
+        'meg':[
+            [0,0],[2,18],[0,0],
+        ],
+        'professor-farnsworth':[
+            [0,0],[4,16],[0,0],
+        ],
+        'quagmire': [
+            [0,0],[6,13],[0,0],
+        ],
+        'teddy':[
+            [0,0],[6,14],[0,0],
+        ], 
+        'amy':[
+            [0,0],[7,20],[8,26],
+        ],
+        'bob':[
+            [0,0],[7,22], [9,24],
+        ],
+        'boomhauer':[
+            [0,0],[6,16],[7,22],
+        ],
+        'brian':[
+            [0,0],[6,20],[7,25],
+        ],
+        'francine':[
+            [0,0],[5,25],[6,31],
+        ],
+        'leela':[
+            [0,0],[4,25],[5,30],
+        ],
+        'louise':[
+            [0,0],[4,21], [5,26],
+        ],
+        'peggy':[
+            [0,0], [8,18],[9,22],
+        ],
+        'stan':[
+            [0,0],[8,15],[8,19],
+        ],
+        'stewie':[
+            [0,0],[4,25],[5,30],
+        ],
+        'bill-dauterive': [
+            [13, 24], [15,30], [17, 37],
+        ],
+        'philip-j-fry':[
+            [12, 23], [15, 29], [18, 38],
+        ],
+        'dr-amy-wong': [
+            [10, 31], [13, 38], [14, 49],
+        ],
+        'eugene-belcher': [
+            [11, 24], [14, 31], [16, 40],
+        ],
+        'chris-griffin': [
+            [11, 26], [14, 33], [14, 42],
+        ],
+        'meg-griffin': [
+            [8, 29], [10, 37], [12, 46],
+        ],
+        'hank-hill': [
+            [12, 21], [14, 29], [15, 36],
+        ],
+        'bender': [
+            [9, 24], [11, 33], [14, 38],
+        ],
+        'steve-smith': [
+            [6, 36], [6, 47], [8, 56],
+        ],
+        'peter': [
+            [6, 29], [8, 36], [11, 45],
+        ],
+        'roger': [
+            [6, 30], [9, 35], [12, 41],
+        ],
+        'tina': [
+            [8, 27], [11, 33], [12, 39],
+        ],
+        'linda-belcher': [
+            [6, 33], [8, 39], [9, 48],
+        ],
+        'klaus-heisler': [
+            [2, 36], [3, 48], [4, 60],
+        ],
+        'bobby': [
+            [6, 27], [8, 36], [9, 44],
+        ],
+        'francine-smith': [
+            [9, 30], [11, 37], [14, 45],
+        ],
+        'lois-griffin': [
+            [8, 29], [11, 38], [13, 48],
+        ],
+        'hermes-conrad': [
+            [11, 23], [14, 30], [17, 38],
+        ],
+        'mythic-stewie': [
+            [9, 51], [12, 62], [0, 0],
+        ],
+        'mythic-quagmire': [
+            [14, 44], [18, 55], [0, 0],
+        ],
+        'mythic-brian': [
+            [10, 50], [14, 62], [0, 0],
+        ],
+        'mythic-stan': [
+            [14, 42], [17, 57], [0,0],
+        ],
+        'mythic-bullock': [
+            [15, 36], [20, 49], [0,0],
+        ],
+        'mythic-hayley':[
+            [12, 44], [16,57], [0,0],
+        ],
+        'mythic-louise': [
+            [8, 54], [12, 63], [0, 0],
+        ],
+        'mythic-bob': [
+            [16, 44], [19, 55], [0, 0],
+        ],
+        'mythic-teddy': [
+            [13, 40], [17, 60], [0,0],
+        ],
+        'mythic-peggy': [
+            [12, 45], [17, 57], [0,0],
+        ],
+        'mythic-boomhauer':[
+            [12, 41], [17, 52], [0,0],
+        ],
+        'mythic-dale': [
+            [13, 42], [17, 55], [0,0],
+        ],
+        'mythic-leela': [
+            [18, 35], [21, 44],[0,0],
+        ],
+        'mythic-dr-zoidberg': [
+            [11,52], [15, 63], [0,0],
+        ],
+        'mythic-professor-farnsworth': [
+            [15, 35], [19, 50], [0,0],
+        ],
+        'mythic-bender': [
+            [15,44],[19, 56], [0,0],
+        ],
+        'mythic-bobby': [
+            [12, 56], [14, 67],[0,0],
+        ],
+        'mythic-peter': [
+            [11, 52], [15, 65], [0,0],
+        ],
+        'mythic-tina': [
+            [16, 45], [20, 54], [0,0],
+        ],
+}
+
+def type_of_ch(ch):
+    if (stats[ch][2][0] == 0):
+        if (stats[ch][0][0] == 0):
+            return '<epic'
+        else:
+            return 'mythic'
+    elif (stats[ch][0][0] == 0):
+        return 'epic'
+    else:
+        return 'legendary'
+
+chs_N0 = len(chs)
+for i in range(0, chs_N0):
+    ch = chs[i]
+    if type_of_ch(chs[i]) == 'legendary':
+        if ch != 'roger':
+            chs.append(ch + '_lvl_18')
+    elif type_of_ch(chs[i]) == 'mythic':
+        chs.append(ch + '_lvl_1')
+
 
 skill_dict = {
     'bodyguard': 'bodyguard',
@@ -74,7 +310,7 @@ skill_factor = {
     'inspire': 1.5, #'motivate',
     'pierce': 0.5, #'jab',
     'poison': 1.5, #'gas',
-    'strike': 1.2, #'punch',
+    'strike': 1.7, #'punch',
     'armored': 1, #'sturdy',
     'berserk': 3, #'crazed',
     'outlast': 0.5, #'recover',
@@ -110,6 +346,7 @@ bges = [#'none',
 ]
 tr = {}
 rec = {}
+score_dict = {}
 
 with open('combos', 'rb') as f:
     data = pickle.load(f)
@@ -157,6 +394,7 @@ with open('combos', 'rb') as f:
 
     debug = False
     verbose = {'bge': 'athletic', 'ch': 'mythic-dr-zoidberg'}
+    BGE_affected = True
     def lack_of_skill_factor(num):
         if num == 3:
             return 1.0
@@ -189,7 +427,8 @@ with open('combos', 'rb') as f:
                         print([1, sk, skill_factor[sk[0]], maxm[sk[0]], delta])
                 if debug:
                     print([cd.slug, cd.skills, combo_value])
-                combo_value *= dual_skill_bonus(cd.skills)
+                #combo_value *= dual_skill_bonus(cd.skills)
+                score_dict[ch + '+' + cd.item_slug] = combo_value
                 combo_values.append(combo_value)
             if debug:
                 print([ch, combo_values])
@@ -197,17 +436,19 @@ with open('combos', 'rb') as f:
                 continue
             combo_values.sort()
             tr[ch] = 0.0
-            n = min(20, len(combo_values))
+            n = 20 #min(20, len(combo_values))
             if n == 0:
                 continue
             total_wt = 0.0
             for i in range(0, n):
-                total_wt += 1/sqrt(i+1)
-                tr[ch] += combo_values[-(i+1)]/sqrt(i+1)
+                wt = 1/sqrt(i+1)
+                total_wt += wt
+                if i < len(combo_values):
+                    tr[ch] += combo_values[-(i+1)] * wt
             tr[ch] /= total_wt
-            print(tr[ch])
-            tr[ch] += len(combo_values) / 100
-            print(len(combo_values)/100)
+            #print(tr[ch])
+            #tr[ch] += len(combo_values) / 100
+            #print(len(combo_values)/100)
         if debug:
             continue
         sorted_x = sorted(tr.items(), key=operator.itemgetter(1))
@@ -222,22 +463,39 @@ with open('combos', 'rb') as f:
                 chd[x[0]] = 0.0
         for ch in chs:
             rec[ch][bge] = ("%.2f"%chd[ch])
-    csv = ',sum of top 7,'
-    md = '| | Sum of top 7 |'
+    csv = ' , BGE Score ,sum of top 7,'
+    md = '| | BGE Score | Sum of top 7 |'
     for bge in bges:
         csv += bge + ','
         md += bge + '|'
     csv += '\n'
-    md += '\n| --- | --- |'
+    md += '\n| --- | --- | --- |'
     for bge in bges:
         md +=' --- |'
     md += '\n|'
     idx = 1
-    def sum_of_top6(ch):
+    def sum_of_top7_BGE(ch):
+        sm = 0.0
+        arr = []
+        sum_of_factors_BGE = 0.0
+        sum_of_factors = 0.0
+        for bge in bges:
+            BGE_factor = 1.0
+            if bge in current_BGE:
+                BGE_factor = 3.0
+            arr.append((float(rec[ch][bge]))*BGE_factor)
+            sum_of_factors_BGE += BGE_factor
+            sum_of_factors += 1.0
+        arr.sort()
+        for i in range(1, 8):
+            sm += arr[-i]
+        return sm * sum_of_factors/sum_of_factors_BGE
+    def sum_of_top7(ch):
         sm = 0.0
         arr = []
         for bge in bges:
-            arr.append((float(rec[ch][bge])))
+            BGE_factor = 1.0
+            arr.append((float(rec[ch][bge]))*BGE_factor)
         arr.sort()
         for i in range(1, 8):
             sm += arr[-i]
@@ -245,13 +503,13 @@ with open('combos', 'rb') as f:
     if not debug:
         ch_srt = []
         for ch in chs:
-            ch_srt.append([ch, sum_of_top6(ch)])
+            ch_srt.append([ch, sum_of_top7_BGE(ch)])
         ch_srt.sort(key = lambda x : x[1], reverse = True)
         for ch_pair in ch_srt:
             ch = ch_pair[0]
             idx += 1
-            csv += ch + ',%f,'%sum_of_top6(ch)
-            md += ch + '| %.2f |'%sum_of_top6(ch)
+            csv += ch + ',%.2f,%.2f'%(sum_of_top7_BGE(ch), sum_of_top7(ch))
+            md += ch + '| %.2f | %.2f |'%(sum_of_top7_BGE(ch), sum_of_top7(ch))
             for bge in bges:
                 csv += rec[ch][bge] + ','
                 md += rec[ch][bge] + '|'
@@ -260,6 +518,8 @@ with open('combos', 'rb') as f:
 
         with open('tb.txt', 'w') as f:
             f.write(md)
+        with open('scores.json', 'w') as f:
+            f.write('data = \'' + json.dumps(score_dict) + '\';')
 
 
 
--- grab.py
+++ grab.py
@@ -3,9 +3,79 @@ import os
 import pickle
 from lxml import etree
 from lxml.html import tostring, fromstring
+import json
 from selenium import webdriver
 
-chs = ['bill-dauterive',
+skill_dict = {
+    'bodyguard': 'bodyguard',
+    'inspire': 'motivate',
+    'pierce': 'jab',
+    'poison': 'gas',
+    'strike': 'punch',
+    'armored': 'sturdy',
+    'berserk': 'crazed',
+    'outlast': 'recover',
+    'invigorate': 'boost',
+    'counter': 'payback',
+    'weaken': 'cripple',
+    'leech': 'leech',
+    'weakenall': 'cripple-all',
+    'barrier': 'shield',
+    'barrierall': 'shield-all',
+    'rallyall': 'cheer-all',
+    'rally': 'cheer',
+    'heal': 'heal',
+    'healall': 'heal-all',
+    'shrapnel': 'bomb',
+    'hijack': 'hijack',
+}
+
+
+downgrade_dict = {}
+upgrade_dict = {
+    'bullock': 'mythic-bullock',
+    'chris': 'chris-griffin',
+    'dale': 'mythic-dale',
+    'dr-zoidberg': 'mythic-dr-zoidberg',
+    'fry': 'philip-j-fry',
+    'hank': 'hank-hill',
+    'linda': 'linda-belcher',
+    'lois': 'lois-griffin',
+    'stevs': 'steve-smith',
+    'bill': 'bill-dauterive',
+    'gene': 'eugene-belcher',
+    'hayley': 'mythic-hayley',
+    'hermes': 'hermes-conrad',
+    'klaus': 'klaus-heisler',
+    #'luanns': 
+    'meg': 'meg-griffin',
+    'professor-farnsworth': 'mythic-professor-farnsworth',
+    'quagmire': 'mythic-quagmire',
+    'teddy': 'mythic-teddy',
+    'amy': 'dr-amy-wong',
+    'bob': 'mythic-bob',
+    'boomhauer': 'mythic-boomhauer',
+    'brian': 'mythic-brian',
+    'francine': 'francine-smith',
+    'leela': 'mythic-leela',
+    'louise': 'mythic-louise',
+    'peggy': 'mythic-peggy',
+    'stan': 'mythic-stan',
+    'stewie': 'mythic-stewie',
+    'bender': 'mythic-bender',
+    'bobby': 'mythic-bobby',
+    'peter': 'mythic-peter',
+    'tina': 'mythic-tina',
+}
+
+for lower in upgrade_dict:
+    downgrade_dict[upgrade_dict[lower]] = lower
+
+chs = [
+        'bullock', 'chris', 'dale', 'dr-zoidberg', 'fry', 'hank', 'linda', 'lois', 'mort', 'stevs',
+        'bill', 'gene', 'hayley', 'hermes', 'klaus', 'luanns', 'meg', 'professor-farnsworth', 'quagmire', 'teddy', 
+        'amy', 'bob', 'boomhauer', 'brian', 'francine', 'leela', 'louise', 'peggy', 'stan', 'stewie',
+        'bill-dauterive',
         'philip-j-fry',
         'dr-amy-wong',
         'eugene-belcher',
@@ -20,6 +90,7 @@ chs = ['bill-dauterive',
         'linda-belcher',
         'klaus-heisler',
         'bobby',
+        'francine-smith',
         'lois-griffin',
         'hermes-conrad',
         'mythic-stewie',
@@ -38,19 +109,292 @@ chs = ['bill-dauterive',
         'mythic-dr-zoidberg',
         'mythic-professor-farnsworth',
         'mythic-bender',
+        'mythic-bobby',
+        'mythic-peter',
+        'mythic-tina',
         ]
 
+stats = {
+        'bullock':[
+            [0,0], [3,15], [0,0],
+        ],
+        'chris':[
+            [0,0], [3,13],[0,0],
+        ],
+        'dale': [
+            [0,0], [2, 13],[0,0],
+        ],
+        'dr-zoidberg':[
+            [0,0], [2,15],[0,0],
+        ],
+        'fry': [
+            [0,0],[6,10],[0,0],
+        ],
+        'hank':[
+            [0,0],[5,8],[0,0],
+        ],
+        'linda':[
+            [0,0],[3,13],[0,0],
+        ],
+        'lois':[
+            [0,0],[5,10],[0,0],
+        ],
+        'mort': [
+            [0,0], [5,10],[0,0],
+        ],
+        'stevs':[
+            [0,0], [4,12], [0,0],
+        ],
+        'bill': [
+            [0,0],[7,13],[0,0],
+        ],
+        'gene':[
+            [0,0],[4,16],[0,0],
+        ],
+        'hayley':[
+            [0,0],[5,15], [0,0],
+        ],
+        'hermes':[
+            [0,0],[5,16],[0,0],
+        ],
+        'klaus':[
+            [0,0],[4,15], [0,0],
+        ],
+        'luanns':[
+            [0,0],[7,16],[0,0],
+        ],
+        'meg':[
+            [0,0],[2,18],[0,0],
+        ],
+        'professor-farnsworth':[
+            [0,0],[4,16],[0,0],
+        ],
+        'quagmire': [
+            [0,0],[6,13],[0,0],
+        ],
+        'teddy':[
+            [0,0],[6,14],[0,0],
+        ], 
+        'amy':[
+            [0,0],[7,20],[8,26],
+        ],
+        'bob':[
+            [0,0],[7,22], [9,24],
+        ],
+        'boomhauer':[
+            [0,0],[6,16],[7,22],
+        ],
+        'brian':[
+            [0,0],[6,20],[7,25],
+        ],
+        'francine':[
+            [0,0],[5,25],[6,31],
+        ],
+        'leela':[
+            [0,0],[4,25],[5,30],
+        ],
+        'louise':[
+            [0,0],[4,21], [5,26],
+        ],
+        'peggy':[
+            [0,0], [8,18],[9,22],
+        ],
+        'stan':[
+            [0,0],[8,15],[8,19],
+        ],
+        'stewie':[
+            [0,0],[4,25],[5,30],
+        ],
+        'bill-dauterive': [
+            [13, 24], [15,30], [17, 37],
+        ],
+        'philip-j-fry':[
+            [12, 23], [15, 29], [18, 38],
+        ],
+        'dr-amy-wong': [
+            [10, 31], [13, 38], [14, 49],
+        ],
+        'eugene-belcher': [
+            [11, 24], [14, 31], [16, 40],
+        ],
+        'chris-griffin': [
+            [11, 26], [14, 33], [14, 42],
+        ],
+        'meg-griffin': [
+            [8, 29], [10, 37], [12, 46],
+        ],
+        'hank-hill': [
+            [12, 21], [14, 29], [15, 36],
+        ],
+        'bender': [
+            [9, 24], [11, 33], [14, 38],
+        ],
+        'steve-smith': [
+            [6, 36], [6, 47], [8, 56],
+        ],
+        'peter': [
+            [6, 29], [8, 36], [11, 45],
+        ],
+        'roger': [
+            [6, 30], [9, 35], [12, 41],
+        ],
+        'tina': [
+            [8, 27], [11, 33], [12, 39],
+        ],
+        'linda-belcher': [
+            [6, 33], [8, 39], [9, 48],
+        ],
+        'klaus-heisler': [
+            [2, 36], [3, 48], [4, 60],
+        ],
+        'bobby': [
+            [6, 27], [8, 36], [9, 44],
+        ],
+        'francine-smith': [
+            [9, 30], [11, 37], [14, 45],
+        ],
+        'lois-griffin': [
+            [8, 29], [11, 38], [13, 48],
+        ],
+        'hermes-conrad': [
+            [11, 23], [14, 30], [17, 38],
+        ],
+        'mythic-stewie': [
+            [9, 51], [12, 62], [0, 0],
+        ],
+        'mythic-quagmire': [
+            [14, 44], [18, 55], [0, 0],
+        ],
+        'mythic-brian': [
+            [10, 50], [14, 62], [0, 0],
+        ],
+        'mythic-stan': [
+            [14, 42], [17, 57], [0,0],
+        ],
+        'mythic-bullock': [
+            [15, 36], [20, 49], [0,0],
+        ],
+        'mythic-hayley':[
+            [12, 44], [16,57], [0,0],
+        ],
+        'mythic-louise': [
+            [8, 54], [12, 63], [0, 0],
+        ],
+        'mythic-bob': [
+            [16, 44], [19, 55], [0, 0],
+        ],
+        'mythic-teddy': [
+            [13, 40], [17, 60], [0,0],
+        ],
+        'mythic-peggy': [
+            [12, 45], [17, 57], [0,0],
+        ],
+        'mythic-boomhauer':[
+            [12, 41], [17, 52], [0,0],
+        ],
+        'mythic-dale': [
+            [13, 42], [17, 55], [0,0],
+        ],
+        'mythic-leela': [
+            [18, 35], [21, 44],[0,0],
+        ],
+        'mythic-dr-zoidberg': [
+            [11,52], [15, 63], [0,0],
+        ],
+        'mythic-professor-farnsworth': [
+            [15, 35], [19, 50], [0,0],
+        ],
+        'mythic-bender': [
+            [15,44],[19, 56], [0,0],
+        ],
+        'mythic-bobby': [
+            [12, 56], [14, 67],[0,0],
+        ],
+        'mythic-peter': [
+            [11, 52], [15, 65], [0,0],
+        ],
+        'mythic-tina': [
+            [16, 45], [20, 54], [0,0],
+        ],
+}
+
+def type_of_ch(ch):
+    if (stats[ch][2][0] == 0):
+        if (stats[ch][0][0] == 0):
+            return '<epic'
+        else:
+            return 'mythic'
+    elif (stats[ch][0][0] == 0):
+        return 'epic'
+    else:
+        return 'legendary'
+
+
+data = {}
+trait_dict = {}
+dic = {}
+
+
 class at_card:
-    def __init__(self, atk, hp):
+    def __init__(self, atk, hp, item_atk, item_hp):
         self.atk = atk
         self.hp = hp
+        self.item_atk = item_atk
+        self.item_hp = item_hp
         self.trait = 'none'
         self.skills = []
         self.slug = ''
+        self.item_slug = ''
     def prt(self):
         print([self.atk, self.hp, self.trait, self.skills])
+    def alt(self, ch):
+        if not(ch in downgrade_dict):
+            #print("%s not in downgrade dict."%ch)
+            return
+        def alt_sub(atk0, hp0, atkm, hpm, atk1, hp1, ch_alt):
+            atk_ratio = (self.item_atk + atkm)/(self.item_atk + atk1)
+            hp_ratio = (self.item_hp + hpm)/(self.item_hp + hp1)
+            cd = at_card(self.atk * atk_ratio, self.hp * hp_ratio, self.item_atk, self.item_hp)
+            cd.trait = self.trait
+            cd.slug = self.slug
+            cd.item_slug = self.item_slug
+            combo_power1 = (self.item_hp + self.item_atk * 3 + hp0 + atk0 * 3)
+            combo_power2 = (self.item_hp + self.item_atk * 3 + hpm + atkm * 3)
+            combo_power3 = (self.item_hp + self.item_atk * 3 + hp1 + atk1 * 3)
+            a = (combo_power2 - combo_power3) / (combo_power1 - combo_power3)
+            b = 1 - a
+            if (ch_alt[-1] == '8') and (ch[0] != 'm'):
+                print(combo_power1, combo_power2, combo_power3, ch, downgrade_dict[ch], ch_alt)
+                print("XX ", a * combo_power1 + b * combo_power3, combo_power2, a, b)
+            #if not (0<a<1):
+            #    print(combo_power1, combo_power2, combo_power3, ch, downgrade_dict[ch], ch_alt)
+            #    assert(0<a<1)
+            #print("XX ", a * combo_power1 + b * combo_power3, combo_power2, a, b)
+            dch = downgrade_dict[ch]
+            for sk in self.skills:
+                if not (dch + '+' + self.item_slug) in dic:
+                    print(dch + '+' + self.item_slug)
+                    continue
+                for skl in dic[dch + '+' + self.item_slug].skills:
+                    if skl[0] == sk[0]:
+                        cd.skills.append([sk[0], skl[1] * a + sk[1] * b])
+                        if (ch_alt[-1] == '8') and (ch[0] != 'm'):
+                            print(skl, sk, cd.skills[-1])
+                            assert(cd.skills[-1][1]>sk[1])
 
-data = {}
+            if not (ch_alt in data):
+                data[ch_alt] = []
+            data[ch_alt].append(cd)
+        [atk0, hp0] = stats[downgrade_dict[ch]][1]
+        [atk1, hp1] = stats[ch][1]
+        ch_alt = ch
+        if type_of_ch(ch) == 'legendary':
+            alt_sub(atk0, hp0, *stats[ch][2], atk1, hp1, ch + '_lvl_18')
+        elif type_of_ch(ch) == 'mythic':
+            alt_sub(atk0, hp0, *stats[ch][0], atk1, hp1, ch + '_lvl_1')
+        if (type_of_ch(ch) == 'mythic') and (type_of_ch(downgrade_dict[ch]) == 'legendary'):
+            alt_sub(atk0, hp0, *stats[downgrade_dict[ch]][2], atk1, hp1, downgrade_dict[ch] + '_lvl_18')
+
 def analyse(ch):
     fn = 'html/' + ch + '.html'
     if os.path.exists(fn):
@@ -70,20 +414,43 @@ def analyse(ch):
     atcds = []
     index = 0
     rarity = ''
+    trait = ''
+    item_atk = 0
+    item_hp = 0
+    item_slug = ''
+    char_rarity = ''
     for card in cards:
         index += 1
         if index % 3 == 1:
+            char_rarity = card.attrib['rarity']
             continue
         if index % 3 == 2:
             rarity = card.attrib['rarity']
+            if 'trait' in card.attrib:
+                trait = card.attrib['trait']
+            else:
+                trait = 'none'
+            item_atk = int(card.find('cb-stats')[0].text)
+            item_hp = int(card.find('cb-stats')[1].text)
+            item_slug = card.attrib['slug']
+            trait_dict[item_slug] = trait
             continue
-        if rarity != 'legendary':
-            continue
+        if char_rarity in ['legendary', 'mythic']:
+            if not (rarity in ['legendary', 'mythic']):
+                continue
+        #if rarity != 'legendary':
+        #    continue
         atcd = at_card(int(card.find('cb-stats')[0].text),
-                int(card.find('cb-stats')[1].text))
+                int(card.find('cb-stats')[1].text), item_atk, item_hp)
         atcd.slug = card.attrib['slug']
+        atcd.item_slug = item_slug
         if 'trait' in card.attrib:
-            atcd.trait = card.attrib['trait']
+            if card.attrib['trait'] != trait:
+                #print([card.attrib['slug'], card.attrib['trait'], trait])
+                pass
+            if trait == 'none':
+                trait = card.attrib['trait']
+            atcd.trait = trait #card.attrib['trait']
         for node in card.find('cb-skills'):
             try:
                 value = int(node.text)
@@ -97,11 +464,33 @@ def analyse(ch):
             except:
                 continue
         atcds.append(atcd)
+        dic[ch + '+' + atcd.item_slug] = atcd
     data[ch] = atcds
 
 for ch in chs:
     analyse(ch)
 
+for ch in chs:
+    if type_of_ch(ch) in ['legendary', 'mythic']:
+        for cd in data[ch]:
+            cd.alt(ch)
+
+skills = {}
+
+for skill in skill_dict:
+    skills[skill_dict[skill]] = {}
+
+for ch in data:
+    for cd in data[ch]:
+        for sk in cd.skills:
+            skills[skill_dict[sk[0]]][ch + '+' + cd.item_slug] = sk[1]
+
 with open('combos', 'wb') as f:
     pickle.dump(data, f)
 
+with open('traits.json', 'w') as f:
+    f.write('tr_data = \'' + json.dumps(trait_dict) + '\';')
+
+
+with open('skills.json', 'w') as f:
+    f.write('sk_data = \'' + json.dumps(skills) + '\';')
